55ca931d2e7fb6bb74cd9cfa810649fefd39780f
Jorge Lopez Valdez <jorge.lopezvz@udlap.mx>
2024-07-13 11:21:05 -0600

    Fixing issues

--- src/main/java/com/ratelimiter/service/impl/RateLimiterImpl.java
+++ src/main/java/com/ratelimiter/service/impl/RateLimiterImpl.java
@@ -1,18 +1,24 @@
 package com.ratelimiter.service.impl;
 
 import com.ratelimiter.service.RateLimiter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class RateLimiterImpl implements RateLimiter {
     private final int regularLimit;
     private final int premiumLimit;
     private final long timeWindowInSeconds;
     private final Map<Long, UserRequestInfo> userRequestMap = new HashMap<>();
 
-    public RateLimiterImpl(int regularLimit, int premiumLimit, long timeWindowInSeconds) {
+    public RateLimiterImpl(
+            @Value("${ratelimiter.regularLimit}") int regularLimit,
+            @Value("${ratelimiter.premiumLimit}") int premiumLimit,
+            @Value("${ratelimiter.timeWindowInSeconds}") long timeWindowInSeconds) {
         this.regularLimit = regularLimit;
         this.premiumLimit = premiumLimit;
         this.timeWindowInSeconds = timeWindowInSeconds;
